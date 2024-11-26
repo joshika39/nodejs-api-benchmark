0ed3a006182e69b3eeef425730be0e67127e9330
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-26 11:51:47 +0100

    improved server

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const http = require('http');
+const http = require("http");
+const url = require("url");
+
+const processEndpoint = (name, method, req, res, callback) => {
+
+  const parsedUrl = url.parse(req.url, true);
+  const endpoint = parsedUrl.pathname;
+  if (endpoint === name && req.method === method) {
+    callback(req, res);
+  }
+};
+
+const rootHandler = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!");
+};
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello from Node.js!');
-    }
+  processEndpoint("/", "GET", req, res, rootHandler);
 });
 
-server.listen(5000, () => {
-    console.log('HTTP server is running on http://localhost:5000/');
-});
+try {
+  server.listen(8000, () => {
+    console.log("HTTP server is running on http://localhost:8000/");
+  });
+} catch (e) {
+  console.error("Something went wrong:", e);
+}
